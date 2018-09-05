a64800ea9e3495acdb3dbc02fa1d903eebc81b7f
Joshua Jesper Krægpøth Ryder <jor@firstagenda.com>
2018-09-05 21:07:52 +0200

    initinit

---
+++ TopSwagCode.WebApi.Identicon/Controllers/IdenIconController.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Jdenticon;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TopSwagCode.WebApi.Identicon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdenIconController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get(string name, int size)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var icon = Jdenticon.Identicon.FromValue(name, size);
+
+                await icon.SaveAsPngAsync(ms);
+
+                ms.Position = 0;
+                return File(ms.ToArray(), "image/png");
+            }
+        }
+
+    }
+}
